417190552b795a438d6d5e8f85a021147fa39567
Benneth Yankey <yankeybenneth@gmail.com>
2024-10-15 09:31:04 +0000

    remove router component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ToolboxComponent } from "./shared";
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolboxComponent],
+  imports: [ToolboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
